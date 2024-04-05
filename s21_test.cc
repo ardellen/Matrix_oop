3b75b0ace6a61da4330ed5c2975213d77bab2521
Ardelle Nullify <ardellen@oa-c4.msk.24-school.ru>
2024-04-05 16:15:20 +0300

    delete old

--- s21_test.cc
+++
@@ -1,6 +0,0 @@
-#include "s21_test.h"
-
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
--- s21_test.h
+++
@@ -1,8 +0,0 @@
-#ifndef SRC_TESTS_TEST_H_
-#define SRC_TESTS_TEST_H_
-
-#include <gtest/gtest.h>
-
-#include "../s21_matrix_oop.h"
-
-#endif  // SRC_TESTS_TEST_H_
